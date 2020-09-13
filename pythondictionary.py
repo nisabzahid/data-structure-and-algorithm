1aa1c4665ce159e8f64b41f924cac502ec356c52
Nisab bin Zahid <nisabzahid20@gmail.com>
2020-09-13 12:21:27 +0600

    dictinary of python solve

---
+++ pythondictionary.py
@@ -0,0 +1,18 @@
+def group_by_owners(files):
+    fileown = {}
+    for key,value in files.items():
+        if value not in fileown:
+            fileown[value] = []
+            fileown[value].append(key)
+        else:
+            fileown[value].append(key)
+            
+    return fileown
+
+if __name__ == "__main__":    
+    files = {
+        'Input.txt': 'Randy',
+        'Code.py': 'Stan',
+        'Output.txt': 'Randy'
+    }   
+    print(group_by_owners(files))
